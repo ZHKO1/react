282b5115a6c7bfc5fd24de1e3a19eacba3664c73
ZHKO1 <zhko11993@gmail.com>
2020-08-24 17:14:11 +0800

    add React技术揭秘.md 第七章 Hooks

---
+++ demo/example3/index.js
@@ -0,0 +1,85 @@
+// 极简 的 Hook 实现
+// https://react.iamkasong.com/hooks/create.html#%E5%B7%A5%E4%BD%9C%E5%8E%9F%E7%90%86
+let workInProgressHook;
+let isMount = true;
+
+const fiber = {
+  memoizedState: null,
+  stateNode: App
+};
+
+function schedule() {
+  workInProgressHook = fiber.memoizedState;
+  const app = fiber.stateNode();
+  isMount = false;
+  return app;
+}
+
+function dispatchAction(queue, action) {
+  const update = {
+    action,
+    next: null
+  }
+  if (queue.pending === null) {
+    update.next = update;
+  } else {
+    update.next = queue.pending.next;
+    queue.pending.next = update;
+  }
+  queue.pending = update;
+
+  schedule();
+}
+
+function useState(initialState) {
+  let hook;
+
+  if (isMount) {
+    hook = {
+      queue: {
+        pending: null
+      },
+      memoizedState: initialState,
+      next: null
+    }
+    if (!fiber.memoizedState) {
+      fiber.memoizedState = hook;
+    } else {
+      workInProgressHook.next = hook;
+    }
+    workInProgressHook = hook;
+  } else {
+    hook = workInProgressHook;
+    workInProgressHook = workInProgressHook.next;
+  }
+
+  let baseState = hook.memoizedState;
+  if (hook.queue.pending) {
+    let firstUpdate = hook.queue.pending.next;
+
+    do {
+      const action = firstUpdate.action;
+      baseState = action(baseState);
+      firstUpdate = firstUpdate.next;
+    } while (firstUpdate !== hook.queue.pending)
+
+      hook.queue.pending = null;
+  }
+  hook.memoizedState = baseState;
+
+  return [baseState, dispatchAction.bind(null, hook.queue)];
+}
+
+function App() {
+  const [num, updateNum] = useState(0);
+
+  console.log(`${isMount ? 'mount' : 'update'} num: `, num);
+
+  return {
+    click() {
+      updateNum(num => num + 1);
+    }
+  }
+}
+
+window.app = schedule();
